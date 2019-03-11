15d35b243956e27d67ccecc1c1d3bfaa96570d1e
Davide Patti <xedivad@gmail.com>
2019-03-11 15:54:06 +0100

    completed errors saving

--- SiC_Turn_ON_Modeling/Simula.m
+++ SiC_Turn_ON_Modeling/Simula.m
@@ -71,12 +71,9 @@ plot(t_sim, VgsH_sim, 'b');
 plot(t_exp_HS, VgsH_exp, 'r.');
 
 % interpolation
-y_new = interp1(t_sim,VgsH_sim,t_exp_HS);
-%plot(t_exp_HS,y_new,'g.');
-
-error = sum(abs(VgsH_exp-y_new'))/length(t_exp_HS);
-
-save("error.txt","error","-ascii");
+VgsH_sim_new = interp1(t_sim,VgsH_sim,t_exp_HS);
+error_VgsH = sum(abs(VgsH_exp-VgsH_sim_new'))/length(t_exp_HS);
+save("error_VgsH.txt","error_VgsH","-ascii");
 
 grid on;
 title(['V_G_S del dispositivo di High Side  [V]']);
@@ -87,6 +84,12 @@ subplot(2,3,2)
 hold on
 plot(t_sim, VdsH_sim, 'b');
 plot(t_exp_HS, VdsH_exp, 'r');
+
+% interpolation
+VdsH_sim_new = interp1(t_sim,VdsH_sim,t_exp_HS);
+error_VdsH = sum(abs(VdsH_exp-VdsH_sim_new'))/length(t_exp_HS);
+save("error_VdsH.txt","error_VdsH","-ascii");
+
 grid on;
 title(['V_D_S del dispositivo di High Side  [V]']);
 xlabel(['t [s]']);
@@ -96,6 +99,11 @@ subplot(2,3,3)
 hold on
 plot(t_sim, IdH_sim, 'b');
 plot(t_exp_HS, IdH_exp, 'r');
+% interpolation
+IdH_sim_new = interp1(t_sim,IdH_sim,t_exp_HS);
+error_IdH = sum(abs(IdH_exp-IdH_sim_new'))/length(t_exp_HS);
+save("error_IdH.txt","error_IdH","-ascii");
+
 grid on;
 title(['I_D del dispositivo di High Side  [A]']);
 xlabel(['t [s]']);
@@ -105,6 +113,11 @@ subplot(2,3,4)
 hold on
 plot(t_sim, VgsL_sim, 'b');
 plot(t_exp_LS, VgsL_exp, 'r');
+% interpolation
+VgsL_sim_new = interp1(t_sim,VgsL_sim,t_exp_LS);
+error_VgsL = sum(abs(VgsL_exp-VgsL_sim_new'))/length(t_exp_LS);
+save("error_VgsL.txt","error_VgsL","-ascii");
+
 grid on;
 title(['V_G_S del dispositivo di Low Side  [V]']);
 xlabel(['t [s]']);
@@ -114,6 +127,11 @@ subplot(2,3,5)
 hold on
 plot(t_sim, VdsL_sim, 'b');
 plot(t_exp_LS, VdsL_exp, 'r');
+% interpolation
+VdsL_sim_new = interp1(t_sim,VdsL_sim,t_exp_LS);
+error_VdsL = sum(abs(VdsL_exp-VdsL_sim_new'))/length(t_exp_LS);
+save("error_VdsL.txt","error_VdsL","-ascii");
+
 grid on;
 title(['V_D_S del dispositivo di Low Side  [V]']);
 xlabel(['t [s]']);
@@ -123,9 +141,18 @@ subplot(2,3,6)
 hold on
 plot(t_sim, IdL_sim, 'b');
 plot(t_exp_LS, IdL_exp, 'r');
+% interpolation
+IdL_sim_new = interp1(t_sim,IdL_sim,t_exp_LS);
+error_IdL = sum(abs(IdL_exp-IdL_sim_new'))/length(t_exp_LS);
+save("error_IdL.txt","error_IdL","-ascii");
+
 grid on;
 title(['I_D del dispositivo di Low Side  [A]']);
 xlabel(['t [s]']);
 hold off
 
 set(f1,'Position',get(0,'ScreenSize'));
+
+error_HL = [error_VgsH error_VdsH error_IdH error_VgsL error_VdsL error_IdL ]
+
+save("error_HL.txt","error_HL","-ascii");
